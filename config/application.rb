8e018c17a1fb590bf93c500b461b6d09428bb98f
Willyh Hariardi <willyh@sleekr.co>
2019-12-13 02:05:28 +0700

    modify

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 
-module MuscleClubRails
+module SehatQTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
